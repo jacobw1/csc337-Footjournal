81aede2d610cc3442da66fedae69d4f94624bd24
Francisco F <franciscofigueroa108@gmail.com>
2021-11-19 00:31:51 -0700

    got other client side started

---
+++ Footjournal/public_html/account/client.js
@@ -0,0 +1,12 @@
+/*
+Dylan Burish, Jacob Williams, Francisco Figueroa
+CSC 337
+Final Project -- Footjournal
+client.js
+**Short desc. of client-side processes**
+*/
+
+//formdata again?
+function makePost(){
+    var postData = new FormData();
+}
