ae8693dccbf7c5fe44555a9b47cfb5cff396bae7
Dylan Burish <60192469+dylanburish2022@users.noreply.github.com>
2021-11-30 13:01:48 -0700

    Update client.js
    Added code to get posts and build HTML div when loading home.html

--- Footjournal/public_html/account/client.js
+++ Footjournal/public_html/account/client.js
@@ -6,7 +6,39 @@ client.js
 **Short desc. of client-side processes**
 */
 
-//formdata again?
-function makePost(){
-    var postData = new FormData();
+
+function getPosts(){
+    console.log("Getting posts in client?");
+    $.ajax({
+        url: '/account/get/posts',
+        method: 'GET',
+        contentType: 'application/json',
+        success: (result) => {
+            console.log(result);
+            var posts = JSON.parse(result);
+            var bigDiv = "<div id='allPosts'>";
+            for (i in posts){
+              console.log(posts[i]);
+            }
+            bigDiv += "<div>";
+            $('#content').html(bigDiv);
+        }
+    });
+}
+
+
+function buildCommentHTMLDiv(post, bool){
+  let str = "<div class='text_post'>";
+  str += "<img src='" + post.profilePicture + "' width='40' height='40'> <br>";
+  str += "<div class='username_post'>" + post.username + "</div>";
+  str += "<div class='content_post'>" + post.body + "<br>";
+  if (bool){
+    str += "<img src='" + post.image + "' width='150' height='150'> <br>";
+  }
+  str += "</div>";
+  str += "<button type='button' class='button_post'>Like</button>"
+  str += "<button type='button' class='button_post'>Comment</button>"
+  str += "<input type='text' id='comment' name='comment' value=''>"
+  str += "</div>";
+  return str;
 }
